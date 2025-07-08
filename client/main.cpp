f133e6782525ed137cee14cb08f0e8bf0a6ff2da
skubicki342 <szymon.kubicki342@gmail.com>
2025-07-08 13:40:55 +0200

    small refactor

--- client/main.cpp
+++ client/main.cpp
@@ -4,24 +4,18 @@
 using namespace std;
 #pragma comment(lib,"ws2_32.lib")
 
-/*
-    initialize winsock
-    create socket
-    connect to the server
-    send/recv
-    close the socket
-
-*/
 bool Initialize(){
     WSADATA data;
     return WSAStartup(MAKEWORD(2,2), &data) == 0;
 }
 
 int main(){
+    //initialization
     if(!Initialize()){
         cout << "initialize winsock failed" << endl;
         return 1;
     }
+    //socket creation
     SOCKET s;
     s = socket(AF_INET, SOCK_STREAM, 0);
     if(s == INVALID_SOCKET){
@@ -29,6 +23,7 @@ int main(){
         return 1;
     }
 
+    //create address structure
     int port = 12345;
     string serveraddress = "127.0.0.1";
     sockaddr_in serveraddr;
@@ -36,7 +31,7 @@ int main(){
     serveraddr.sin_port = htons(port);
     inet_pton(AF_INET,serveraddress.c_str(),&(serveraddr.sin_addr));
 
-
+    //connect
     if(connect(s, reinterpret_cast<sockaddr*>(&serveraddr), sizeof(serveraddr)) == SOCKET_ERROR){
         cout << "not able to connect to server" << endl;
         closesocket(s);
@@ -46,14 +41,16 @@ int main(){
     cout << "succesfully connected to server" << endl;
 
     //send/recv
-
     cout << "client program started" << endl;
     string message = "hello there!";
+
     int bytesent;
     bytesent = send(s,message.c_str(), message.length(),0);
     if(bytesent == SOCKET_ERROR){
         cout << "send failed" << endl;
     }
+
+    //closing
     closesocket(s);
     WSACleanup();
     return 0;
